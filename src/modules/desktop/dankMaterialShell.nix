0bffff20bf3da73c8b1ce546c4f41c3be68ebd68
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-07 19:53:38 -0700

    test

--- src/modules/desktop/dankMaterialShell.nix
+++ src/modules/desktop/dankMaterialShell.nix
@@ -5,17 +5,17 @@
   {
     imports = [
       inputs.dms.homeModules.dankMaterialShell.default
-      inputs.dms.homeModules.dankMaterialShell.niri
+      # inputs.dms.homeModules.dankMaterialShell.niri
     ];
 
-    programs.niri.enable = true;
+    # programs.niri.enable = true;
 
     programs.dankMaterialShell = {
       enable = true;
-      niri = {
-        enableKeybinds = true;   # Sets static preset keybinds
-        enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
-      };
+      # niri = {
+      #   enableKeybinds = true;   # Sets static preset keybinds
+      #   enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
+      # };
     };
   };
 }
